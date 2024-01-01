3e98f960b1f8c764df2c00fe9e993f47e318f4c7
Rupa Lahiri <rupacoach@gmail.com>
2024-01-01 18:54:22 +0530

    Add terraform code for create index with mapping

---
+++ aws_opensearch_index_mapping/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    opensearch = {
+      source = "opensearch-project/opensearch"
+      version = "2.1.0"
+    }
+  }
+}
+
+provider "opensearch" {
+  url = "url"
+  healthcheck       = "false"
+  aws_region        = "region"
+  username          = "username"
+  password          = "password"
+  sign_aws_requests = "false" 
+  version_ping_timeout = "10"
+
+}
+
+resource "opensearch_index" "index" {
+  name = "sample"
+  number_of_replicas = "1"
+  number_of_shards = "1"
+  mappings = jsonencode({
+    "properties": {
+      "age": {
+        "type": "integer"
+      }
+    }
+  })
+}
+
+
